7778fccee98d3227ab47310b14c1b034b71c4c6f
NasiadkaMaciej <maciej.nasiadka@gmail.com>
2022-10-17 11:54:06 +0200

    Add opacity and RGB

--- app/src/main/java/com/example/photorgb/MainActivity.java
+++ app/src/main/java/com/example/photorgb/MainActivity.java
@@ -2,25 +2,73 @@ package com.example.photorgb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    SeekBar RedSB, GreenSB, BlueSB;
+    ImageView image;
+    SeekBar WhiteSB, RedSB, GreenSB, BlueSB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RedSB = findViewById(R.id.seekBar1);
-        GreenSB = findViewById(R.id.seekBar2);
-        BlueSB = findViewById(R.id.seekBar3);
-    }
-
-    public void RGBchange(View view) {
-
+        image = findViewById(R.id.imageView);
+        WhiteSB = findViewById(R.id.seekBar1);
+        RedSB = findViewById(R.id.seekBar2);
+        GreenSB = findViewById(R.id.seekBar3);
+        BlueSB = findViewById(R.id.seekBar4);
+        WhiteSB.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                image.setColorFilter((Color.argb(WhiteSB.getProgress(), RedSB.getProgress(), GreenSB.getProgress(), BlueSB.getProgress())));
+            }
+        });
+        RedSB.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                image.setColorFilter((Color.argb(WhiteSB.getProgress(), RedSB.getProgress(), GreenSB.getProgress(), BlueSB.getProgress())));
+            }
+        });
+        GreenSB.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                image.setColorFilter((Color.argb(WhiteSB.getProgress(), RedSB.getProgress(), GreenSB.getProgress(), BlueSB.getProgress())));
+            }
+        });
+        BlueSB.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                image.setColorFilter((Color.argb(WhiteSB.getProgress(), RedSB.getProgress(), GreenSB.getProgress(), BlueSB.getProgress())));
+            }
+        });
     }
 }
